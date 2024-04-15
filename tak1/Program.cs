c26f22375e4e22c790e44c1c1b26903be2c422b7
insanebeach <147543067+insanebeach@users.noreply.github.com>
2024-04-15 13:39:55 +0400

    123
    Complete

---
+++ tak1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void PrintNumbers(int M, int N)
+    {
+        if (M <= N)
+        {
+            Console.WriteLine(M);
+            PrintNumbers(M + 1, N);
+        }
+    }
+
+    static void Main()
+    {
+        int M = 1;  // Установите значение M
+        int N = 10;  // Установите значение N
+
+        PrintNumbers(M, N);
+    }
+}
---
+++ task3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте произвольный массив.
+//Выведете его элементы, начиная с конца. Использовать рекурсию, 
+//не использовать циклы.
+
+using System;
+
+class Program
+{
+    static void PrintArrayReverse(int[] arr, int index)
+    {
+        if (index < 0)
+        {
+            return;
+        }
+
+        Console.Write(arr[index] + " ");
+        PrintArrayReverse(arr, index - 1);
+    }
+
+    static void Main()
+    {
+        int[] array = { 1, 2, 3, 4, 5 }; // Произвольный массив
+
+        Console.WriteLine("Элементы массива в обратном порядке:");
+        PrintArrayReverse(array, array.Length - 1);
+    }
+}
